a4af3a0b361bd7c0e294c177259e6d82df6e3771
Tasmin Afroz <tasmin.afroz@bjitgroup.com>
2018-03-02 16:31:03 +0600

    first commit

---
+++ src/main/java/com/bjit/controller/EmployeeController.java
@@ -0,0 +1,124 @@
+package com.bjit.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bjit.model.Country;
+import com.bjit.model.Department;
+import com.bjit.model.Employee;
+import com.bjit.model.JobTitle;
+import com.bjit.repository.CountryRepository;
+import com.bjit.repository.DepartmentRepository;
+import com.bjit.repository.EmployeeRepository;
+import com.bjit.repository.JobRepository;
+import com.bjit.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+	@Autowired
+	CountryRepository countryRepository;
+
+	@Autowired
+	DepartmentRepository departmentRepository;
+
+	@Autowired
+	JobRepository jobRepository;
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	public EmployeeController(EmployeeService accountServiceMock) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 * @param employee
+	 * @param country
+	 * @param model
+	 * @return
+	 */
+//	@RequestMapping("/hi")
+//	public String newPage(@ModelAttribute Employee employee, @ModelAttribute Country country, Model model) {
+//		//
+//		List<Country> countryList = (List<Country>) countryRepository.findAll();
+//		for (int i = 0; i < 6; i++) {
+//			System.out.println(countryList.get(i).getCountryName());
+//		}
+//		model.addAttribute("employee", new Employee());
+//		model.addAttribute("countryList", countryList);
+//		//
+//
+//		return "employeeForm";
+//		// return "hi";
+//	}
+
+	@RequestMapping("/list")
+	public String homePage() {
+		return "homePage";
+	}
+
+	/**
+	 * Show create form to create new employee
+	 * 
+	 * @param employee
+	 * @param country
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/create")
+	public String employeeForm(@ModelAttribute Employee employee, Model model) {
+		List<Country> countryList = (List<Country>) countryRepository.findAll();
+		List<Employee> employeeList = (List<Employee>) employeeRepository.findAll();
+		// List<Employee> managerList= coachList;
+		List<Department> departmentList = (List<Department>) departmentRepository.findAll();
+		List<JobTitle> jobTitleList = (List<JobTitle>) jobRepository.findAll();
+		model.addAttribute("employee", new Employee());
+		model.addAttribute("countryList", countryList);
+		model.addAttribute("employeeList", employeeList);
+		// model.addAttribute("managerList", employeeList);
+
+		model.addAttribute("departmentList", departmentList);
+		model.addAttribute("jobTitleList", jobTitleList);
+		return "employeeForm";
+	}
+
+	@RequestMapping("/details/{id}")
+	public String employeeDetails(@PathVariable("id") int id, Model model) {
+		Employee employee = employeeRepository.findOne(id);
+		if (employee == null) {
+			return "redirect:/employee/create";
+		}
+		List<Country> countryList = (List<Country>) countryRepository.findAll();
+		List<Employee> employeeList = (List<Employee>) employeeRepository.findAll();
+		// List<Employee> managerList= coachList;
+		List<Department> departmentList = (List<Department>) departmentRepository.findAll();
+		List<JobTitle> jobTitleList = (List<JobTitle>) jobRepository.findAll();
+		model.addAttribute("employee", employee);
+		model.addAttribute("countryList", countryList);
+		model.addAttribute("employeeList", employeeList);
+		// model.addAttribute("managerList", employeeList);
+
+		model.addAttribute("departmentList", departmentList);
+		model.addAttribute("jobTitleList", jobTitleList);
+
+		return "employeeForm";
+	}
+
+	// @RequestMapping("/hi")
+	// public String newPage() {
+	// return "employeeForm";
+	// }
+	//
+	// @RequestMapping("/hi")
+	// public String newPage() {
+	// return "employeeForm";
+	// }
+}
---
+++ src/test/java/com/example/StudentControllerTest.java
@@ -0,0 +1,247 @@
+package com.example;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.bjit.controller.EmployeeController;
+import com.bjit.model.Country;
+import com.bjit.model.Department;
+import com.bjit.model.Employee;
+import com.bjit.model.JobTitle;
+import com.bjit.repository.EmployeeRepository;
+import com.bjit.service.EmployeeService;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+public class StudentControllerTest {
+	@Mock
+	EmployeeService employeeService;
+
+	
+	@Mock
+	Page limitPage ;
+	 @Mock
+	 DataTablesInput dbInput;
+	
+	@Mock
+	Employee employee;
+	@Mock
+	Country country;
+	@Mock
+	Department department;
+	@Mock
+	JobTitle job;
+
+	@Mock
+	ModelAttribute modelAttribute;
+
+	@Mock
+	Model model;
+
+	MockMvc mockMvc;
+	PageRequest limit;
+	@InjectMocks
+	EmployeeController employeeController;
+	
+	@Mock
+	EmployeeRepository employeeRepository;
+
+	private String showList = "http://localhost:8080/employee/list";
+
+	private String saveEmployee = "http://localhost:8080/api/employee/save";
+
+	private String updateEmployee = "http://localhost:8080/api/employee/update";
+
+	@Before
+	public void setUp() {
+		mockMvc = MockMvcBuilders.standaloneSetup(new EmployeeController(employeeService)).build();
+	}
+
+	@Test
+	public void test_get_all_success() throws Exception {
+		List<Employee> users = Arrays.asList(new Employee(1, "Daenerys Targaryen"), new Employee(2, "John Snow"));
+
+		// Page<Employee> user =
+
+//		when(employeeService.findAll(limit)).thenReturn((Page<Employee>) users);
+		
+		
+		when(employeeRepository.findAll()).thenReturn((List<Employee>) users);
+
+		mockMvc.perform(get("api/employee/list")).andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(jsonPath("$", hasSize(2))).andExpect(jsonPath("$[0].employeeId", is(1)))
+				.andExpect(jsonPath("$[0].name", is("Daenerys Targaryen")))
+				.andExpect(jsonPath("$[1].employeeId", is(2))).andExpect(jsonPath("$[1].name", is("John Snow")));
+
+		verify(employeeService, times(1)).findAll(limit);
+		verifyNoMoreInteractions(employeeService);
+	}
+
+	@Test
+	public void test_get_by_id_success() throws Exception {
+		Employee user = new Employee(1, "Daenerys Targaryen");
+		when(employeeService.findById(1)).thenReturn(user);
+		mockMvc.perform(get("api/employee/details/{id}", 1)).andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(jsonPath("$.employeeId", is(1))).andExpect(jsonPath("$.name", is("Daenerys Targaryen")));
+		verify(employeeService, times(1)).findById(1);
+		verifyNoMoreInteractions(employeeService);
+	}
+
+	// Checking the negative response for findById of resController
+	@Test
+	public void test_get_by_id_fail_404_not_found() throws Exception {
+		when(employeeService.findById(1)).thenReturn(null);
+		mockMvc.perform(get("api/employee/details/{id}", 1)).andExpect(status().isNotFound());
+		verify(employeeService, never()).findById(1);
+		verifyNoMoreInteractions(employeeService);
+	}
+
+	
+	
+//	test employee creation
+	@Test
+	public void testSaveEmployee() {
+		doNothing().when(employeeService).saveEmployee(employee);
+		
+	}
+	
+	
+	
+	@Test
+	public void testFindAll() {
+		doNothing().when(employeeService).findAll(limit);
+		
+	}
+	
+	
+	
+	// does "/employee/list" URL gets hitted
+	@Test
+	public void testHomePage() throws IOException {
+
+		assertNotNull(employeeController.homePage());
+
+	}
+
+	// url to show list requestmapping get
+	@Test
+	public void testIfGet() throws Exception {
+		// assertNotNull(mainController.InvoiceList(modelMock));
+		mockMvc.perform(get(showList)).andExpect(status().isOk());
+		// mockMvc.perform(post(saveEmployee)).andExpect(status().isOk());
+
+	}
+
+	// url t0 save epmloyee is requestmapping post
+	@Test
+	public void testIfPost() throws Exception {
+		// assertNotNull(mainController.InvoiceList(modelMock));
+		// mockMvc.perform(get(showList)).andExpect(status().isOk());
+		mockMvc.perform(post(saveEmployee)).andExpect(status().isOk());
+
+	}
+
+	// url t0 update epmloyee is requestmapping put
+	@Test
+	public void testIfPut() throws Exception {
+		// assertNotNull(mainController.InvoiceList(modelMock));
+		// mockMvc.perform(get(showList)).andExpect(status().isOk());
+		mockMvc.perform(put(updateEmployee)).andExpect(status().isOk());
+
+	}
+
+	@Test
+	public void testEmployeeForm() throws IOException {
+		model.addAttribute(country);
+		model.addAttribute(department);
+		model.addAttribute(job);
+		model.addAttribute(employee);
+		assertNotNull(employeeController.employeeForm(employee, model));
+
+	}
+
+	@Test
+	public void TestEmployeeDetails() throws IOException {
+
+		assertNotNull(employeeController.employeeDetails(2, model));
+
+	}
+
+	// @BeforeClass
+	// public static void beforeLeaveRepositoryTest() {
+	// leave = new Leave();
+	//// invoice.getCustomer().setName("Test Customer");
+	//// invoice.setStatus("Open");
+	// leave.setCommentByManager("asdas");
+	// leave.setLeaveId(1);
+	// leave.setDescription("asd");
+	// leave.setLeaveType("asd");
+	// leave.setLeaveEndDate(new Date(333333));
+	// leave.setLeaveStatus("abc");
+	// leave.setLeaveStartDate(new Date(22222));
+	// Employee emp = new Employee();
+	// emp.setEmployeeId(1);
+	// emp.setEmployeeName("Masud");
+	// Department dept = new Department();
+	// dept.setDepartment("Ict");
+	// emp.setDepartment(dept);
+	// leave.setEmployee(emp);
+	//
+	//
+	// }
+	//
+	// @AfterClass
+	// public static void afterLeaveRepositoryTest() {
+	//
+	// }
+
+	@Test
+	public void shouldGetEmployeeById() throws Exception {
+
+		Employee user = new Employee(1, "Daenerys Targaryen");
+		when(employeeService.findById(1)).thenReturn(user);
+		// when(employeeService.findById((1)).thenReturn(user);
+		//
+		// Integer id = 1;
+		// Leave leave1 = leaveRepo.findOne(id);
+		// System.out.println(leave1);
+		// System.out.println(leave);
+		// assertEquals(leave, leave1);
+	}
+}
